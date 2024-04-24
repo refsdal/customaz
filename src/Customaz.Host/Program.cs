09be994b0d4f56b99d3adaed765aa092433a96b5
Anders Refsdal Olsen <andersro93@gmail.com>
2024-04-24 21:28:31 +0200

    chore(setup): convert host project to web application

--- src/Customaz.Host/Program.cs
+++ src/Customaz.Host/Program.cs
@@ -1,3 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿var builder = WebApplication.CreateSlimBuilder(args);
 
-Console.WriteLine("Hello, World!");
+var app = builder.Build();
+
+app.Run();
