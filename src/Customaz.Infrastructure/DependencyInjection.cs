3a680781cc0e4f00271bc53ee5cf0c0b3e12b245
Anders Refsdal Olsen <andersro93@gmail.com>
2024-04-24 21:39:26 +0200

    chore(setup): add dependency injection for infrastructure

---
+++ src/Customaz.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,11 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Customaz.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection serviceCollection)
+    {
+        return serviceCollection;
+    }
+}
